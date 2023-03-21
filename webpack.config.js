630be42344798016d0227b67c281bd35b9681905
BobSlim <63500133+BobSlim@users.noreply.github.com>
2023-03-21 22:56:54 +1100

    restart

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,9 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
-  },
-  devtool: 'inline-source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean:true,
   },
 };
